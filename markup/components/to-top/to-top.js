ffec1b58f254db22c11ebaae050ea015b607deb1
Alex Faad <alexufiik@gmail.com>
2018-08-10 11:54:05 +0000

    c

---
+++ markup/components/to-top/to-top.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function () {
+    let toTopBtn = document.getElementById('to-top');
+
+    document.addEventListener('scroll', function () {
+        if (pageYOffset > 1000) {
+            toTopBtn.classList.add('to-top__show');
+        } else {
+            toTopBtn.classList.remove('to-top__show');
+        }
+    });
+
+    toTopBtn.addEventListener('click', function() {
+        scrollTo(0, 0);
+    });
+});
